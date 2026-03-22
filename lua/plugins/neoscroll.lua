356d5d795bb2a72f209a5be8c205323fde35286e
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-22 03:17:55 -0400

    clean up init.lua

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,24 +1,33 @@
+-- NEOSCROLL
 return {
-  "karb94/neoscroll.nvim",
-  event = "VeryLazy",  -- only loads when stuff has settled in
+    "karb94/neoscroll.nvim",
+    event = "VeryLazy", -- only loads when essentials are there
     config = function()
-      require("neoscroll").setup({
-        mappings = {
-          "<C-u>", "<C-d>",
-          "<C-b>", "<C-f>",
-          "<C-y>", "<C-e>",
-          "zt", "zz", "zb",
-        },
-        hide_cursor = true,
-        stop_eof = true,
-        respect_scrolloff = false,
-        cursor_scrolls_alone = true,
-        duration_multiplier = 1.0,
-        easing = "linear",
-        pre_hook = nil,
-        post_hook = nil,
-        performance_mode = false,
-        ignored_events = { "WinScrolled", "CursorMoved" },
-      })
+        local neoscroll = require("neoscroll")
+
+        neoscroll.setup({
+            mappings = {
+                "<C-u>", "<C-d>",
+                "<C-b>", "<C-f>",
+                "<C-y>", "<C-e>",
+                "zt", "zz", "zb",
+            },
+            hide_cursor = true,
+            stop_eof = true,
+            respect_scrolloff = false,
+            cursor_scrolls_alone = true,
+            duration_multiplier = 1.0,
+            easing = "linear",
+        })
+
+        local keymap = {
+            ["<Up>"] = function() neoscroll.ctrl_u({ duration = 200 }) end,
+            ["<Down>"] = function() neoscroll.ctrl_d({ duration = 200 }) end,
+        }
+
+        local modes = { "n", "v", "x" }
+        for key, func in pairs(keymap) do
+            vim.keymap.set(modes, key, func)
+        end
     end,
 }
--- lua/plugins/smear_cursor.lua
+++ lua/plugins/smear_cursor.lua
@@ -1,6 +1,31 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-	  time_interval = 5, -- ms
+    -- General settings
+    cursor_color = "#B02EBF",
+    time_interval = 5,
+    stiffness = 0.8,
+    trailing_stiffness = 0.2,
+    stiffness_insert_mode = 0.7,
+    trailing_exponent = 5,
+    damping = 0.85,
+    damping_insert_mode = 0.95,
+    distance_stop_animating = 0.5,
+    gradient_exponent = 0,
+    gamma = 1,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+
+    -- Particle settings
+    particles_enabled = true,
+    particle_spread = 1,
+    particles_per_second = 500,
+    particles_per_length = 50,
+    particle_max_lifetime = 800,
+    particle_max_initial_velocity = 20,
+    particle_velocity_from_cursor = 0.5,
+    particle_damping = 0.15,
+    particle_gravity = -50,
+    min_distance_emit_particles = 0,
   },
 }
